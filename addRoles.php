32af900acabed7012be1f46c96b2949a853d646a
Segundo Galdames <segundogaldames@gmail.com>
2021-04-20 12:26:13 -0300

    Procesamiento de formulario addRoles.php

--- addRoles.php
+++ addRoles.php
@@ -1,3 +1,20 @@
+<?php
+    //validamos que el formulario sea enviado via post
+    if (isset($_POST['confirm']) && $_POST['confirm'] == 1) {
+
+        //guardaremos el nombre del rol en la variable nombre
+        $nombre = trim(strip_tags($_POST['nombre'])); //sanitizar la variable nombre
+
+        //validar que la variable no este vacia
+        if (!$nombre) {
+            $msg = 'Debe ingresar el nombre del rol';
+        }
+
+        //la funcion print_r permite imprimir datos a manera de prueba
+        //print_r($nombre);exit;
+    }
+?>
+
 <!--Estructura del DOM (Document Object Model)-->
 <!DOCTYPE html>
 <html lang="es">
@@ -5,9 +22,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Nuevo Rol</title>
-    
+    <title>Mi Primera Página</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
@@ -21,19 +36,27 @@
     </header>
     <!-- cuerpo central de la pagina web -->
     <section>
+        <h1>Nuevo Rol</h1>
         <div class="formulario">
-            <h1>Nuevo Rol</h1>
+           <!--  GET => envia datos a traves de la url del navegador (URI) al servidor
+            POST => envia datos de manera interna al servidor -->
             <form action="" method="post">
                 <div class="form-group">
-                    <label for="">Nombre:</label>
-                    <input type="text" name="nombre" placeholder="Ingrese el nombre del rol" class="form-control">
+                    <label for="rol">Rol</label>
+                    <input type="text" name="nombre" class="form-control" placeholder="Ingrese el nombre del rol">
+                    <?php if(isset($msg)): ?>
+                        <p class="text-danger">
+                            <?php echo $msg; ?>
+                        </p>
+                    <?php endif; ?>
                 </div>
                 <div class="form-group">
+                    <input type="hidden" name="confirm" value="1">
                     <button type="submit" class="btn btn-primary">Guardar</button>
                 </div>
-                
             </form>
         </div>
+        
         
     </section> 
     <!-- pie de pagina del sitio -->
@@ -43,4 +66,3 @@
     
 </body>
 </html>
-
